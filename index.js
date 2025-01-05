38719ea9afe96913d80f37bedf0945255a164662
Niiii <tapioka2real@gmail.com>
2025-01-06 02:54:39 +0900

    voice

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { default: axios } = require("axios");
-const rpc = axios.create({baseURL: "http://127.0.0.1:50021", proxy: false, });
+const rpc = axios.create({baseURL: "http://localhost:50021", proxy: false, });
 const { getVoiceConnection, createAudioResource, StreamType, createAudioPlayer, NoSubscriberBehavior, generateDependencyReport } = require("@discordjs/voice");
 const fs = require('fs')
 
